b45810563779e896d919fb6cc607a4643311ddd2
Sidney Marcelo Franco <sidneymarcelofranco@hotmail.com>
2022-06-24 15:12:02 -0300

    Add project files.

---
+++ JsonReaderDataSet/DataSet.cs
@@ -0,0 +1,27 @@
+﻿
+internal class DataSet
+{
+    public List<item> itens { get; set; }
+
+    public class item
+    {
+        public string chassi { get; set; }
+        public string cor_do_veiculo { get; set; }
+        public string desc_cor { get; set; }
+        public int ano_f { get; set; }
+        public int ano_m { get; set; }
+        public string motor { get; set; }
+        public string tracao { get; set; }
+        public string versao_do_veiculo { get; set; }
+        public string desc_modelo { get; set; }
+        public string cnpj_do_br { get; set; }
+        public string modelo { get; set; }
+        public string data_venda { get; set; }
+        public int nf { get; set; }
+        public int valor_venda_kia { get; set; }
+
+    }
+
+}
+
+
---
+++ JsonReaderDataSet/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+
+
+string strJSON = @"{'itens':
+[{
+	'chassi':'9UWSHX73ABN001138',
+	'cor_do_veiculo':'UU',
+	'desc_cor':'BRANCO',
+	'ano_f':2000,
+	'ano_m':2011,
+	'motor':'D4BHA056400',
+	'tracao':'4X2',
+	'versao_do_veiculo':'K77211',
+	'desc_modelo':'UK2500 HD SC',
+	'cnpj_do_br':'08715766000326',
+	'modelo':'UK2500',
+	'data_venda':'25/02/2011',
+	'nf':207500,
+	'valor_venda_kia':48510},
+{
+	'chassi':'9UWSHX73ABN001138',
+	'cor_do_veiculo':'UU',
+	'desc_cor':'BRANCO',
+	'ano_f':2000,
+	'ano_m':2011,
+	'motor':'D4BHA056400',
+	'tracao':'4X2',
+	'versao_do_veiculo':'K77211',
+	'desc_modelo':'UK2500 HD SC',
+	'cnpj_do_br':'08715766000326',
+	'modelo':'UK2500',
+	'data_venda':'25/02/2011',
+	'nf':207500,
+	'valor_venda_kia':48510}
+]}";
+
+
+
+var dataSet = JsonConvert.DeserializeObject<DataSet>(strJSON);
+
+//DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
+
+//DataTable dataTable = dataSet.Tables["Table1"];
+
+//Console.WriteLine(dataTable.Rows.Count);
+// 2
+
+foreach (var item in dataSet.itens)
+{
+	Console.WriteLine("Class" + item.chassi.ToString());
+	Console.WriteLine("cor do veiculo" + item.cor_do_veiculo.ToString());
+	Console.WriteLine("Desc Cor" + item.desc_cor.ToString());
+	Console.WriteLine("Ano F" + item.ano_f.ToString());
+	Console.WriteLine("Ano M" + item.ano_m.ToString());
+	Console.WriteLine("Motor" + item.motor.ToString());
+	Console.WriteLine("Tração" + item.tracao.ToString());
+	Console.WriteLine("Versão do Veículo" + item.versao_do_veiculo.ToString());
+	Console.WriteLine("Desc modelo" + item.desc_modelo.ToString());
+	Console.WriteLine("CNPJ do BR" + item.cnpj_do_br.ToString());
+	Console.WriteLine("Data Venda" + item.data_venda.ToString());
+	Console.WriteLine("NF" + item.nf.ToString());
+	Console.WriteLine("Valor Venda Kia" + item.valor_venda_kia.ToString());
+
+}
+
+
+
